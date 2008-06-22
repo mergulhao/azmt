28e4e245c966ce919a47223adbe8987bef03de38
mergulhao <mergulhao@76623a5a-d52f-0410-8f39-fa83289919e8>
2008-06-22 19:10:38 +0000

    git-svn-id: svn+vpsssh://en21.org/svn/azmt/trunk@219 76623a5a-d52f-0410-8f39-fa83289919e8

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,5 +6,10 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '4540880ff58d9b82babbd2ea27cbee65'
+  protect_from_forgery # :secret => '711bafc0d3a34870160f938fbb75efe5'
+  
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  # filter_parameter_logging :password
 end
