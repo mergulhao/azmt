0be55e8378acf89e8ad4febff941f4b0368c552e
mergulhao <mergulhao@76623a5a-d52f-0410-8f39-fa83289919e8>
2008-07-01 06:26:55 +0000

    finalizado script de deploy
    git-svn-id: svn+vpsssh://en21.org/svn/azmt/trunk@257 76623a5a-d52f-0410-8f39-fa83289919e8

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,20 +4,19 @@
 # Email: contato (at) mergulhao.info
 # Site: http://mergulhao.info
 
-set :svn_base_address, "svn+vpsssh://en21.org/svn/azmt"
-
-def get_next_release
-  list = `svn list #{svn_base_address}/branches | grep RB-`
+def next_release
+  return @next_release if @next_release
+  list = `svn list #{svn_base_remote_address}/branches | grep RB-`
   list_numbers = []
   list.each do |item|
     item =~ /RB-([0-9].*)\//
     list_numbers << $1.to_i
   end
-  next_release = list_numbers.sort.last.to_i + 1
-  system "svn copy -m \"Creating release branch\" #{svn_base_address}/trunk #{svn_base_address}/branches/RB-#{next_release}"
-  system "svn copy -m \"Creating tag release\" #{svn_base_address}/branches/RB-#{next_release} #{svn_base_address}/tags/REL-#{next_release}"
-  next_release
+  @next_release = list_numbers.sort.last.to_i + 1
 end
+
+set :svn_base_remote_address, "svn+vpsssh://en21.org/svn/azmt"
+set :svn_base_local_address,  "file:///svn/azmt"
 
 # Define o nome da aplicacao e o dominio a qual estara linkada no dreamhost
 set :application, "azmt.en21.org" 
@@ -27,10 +26,8 @@ set :user, "railsapp"
 set :port, 20021 
 
 # Define o local do seu repositorio svn
-set :local_repository,  "#{svn_base_address}/trunk/"
-set :repository,  "file:///svn/azmt/tags/REL-#{get_next_release}"
-
-# Nao precisa ser  alterado daqui pra baixo!
+set :local_repository, "#{svn_base_remote_address}/tags/REL-#{next_release}"
+set :repository,       "#{svn_base_local_address}/tags/REL-#{next_release}"
 
 set :use_sudo, false
 set :deploy_to, "/home/#{user}/#{application}" 
@@ -43,8 +40,14 @@ role :db,  domain, :primary => true
 set :lsws_cmd, "/opt/lsws/bin/lswsctrl"
 
 namespace :deploy do
+  task :before_update_code, :roles => :app do
+    system "svn copy -m \"Creating release branch\" #{svn_base_remote_address}/trunk #{svn_base_remote_address}/branches/RB-#{next_release}"
+    system "svn copy -m \"Creating tag release\" #{svn_base_remote_address}/branches/RB-#{next_release} #{svn_base_remote_address}/tags/REL-#{next_release}"
+  end
+  
   desc "Restarts all FCGI processes." 
   task :restart, :roles => :app do
     sudo "#{lsws_cmd} restart"
   end
 end
+
