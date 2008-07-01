fe23ac4b6a43da31b3605e1d04810bd76816828f
mergulhao <mergulhao@76623a5a-d52f-0410-8f39-fa83289919e8>
2008-07-01 04:44:40 +0000

    mais testes para passar na cobertura
    git-svn-id: svn+vpsssh://en21.org/svn/azmt/trunk@230 76623a5a-d52f-0410-8f39-fa83289919e8

--- spec/models/discipline_spec.rb
+++ spec/models/discipline_spec.rb
@@ -1,11 +1,17 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Discipline do
+  fixtures :disciplines
+  
   before(:each) do
-    @discipline = Discipline.new
+    @discipline = disciplines(:math)
   end
 
   it "should be valid" do
     @discipline.should be_valid
+  end
+  
+  it "should return name" do
+    @discipline.to_s.should eql("Math")
   end
 end
