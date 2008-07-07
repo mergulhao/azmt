9610f713ccc74530dcfb3b9dbab6dc34fa1546e1
mergulhao <mergulhao@76623a5a-d52f-0410-8f39-fa83289919e8>
2008-07-07 06:31:59 +0000

    adicionando testes no classroom
    git-svn-id: svn+vpsssh://en21.org/svn/azmt/trunk@275 76623a5a-d52f-0410-8f39-fa83289919e8

--- spec/models/classroom_spec.rb
+++ spec/models/classroom_spec.rb
@@ -1,11 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Classroom do
+  fixtures :classrooms
+  
   before(:each) do
     @classroom = Classroom.new
   end
 
   it "should be valid" do
     @classroom.should be_valid
+  end
+
+  describe ".to_s" do
+    it "should return the classroom name" do 
+      classrooms(:amazonia).to_s.should eql('Amazônia')
+      classrooms(:amazonia).name.should eql('Amazônia')
+    end
   end
 end
