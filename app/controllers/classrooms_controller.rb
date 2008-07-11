9de99d47fbe2d4ba8b1e3322c321ca64c481023b
mergulhao <mergulhao>
2008-07-11 05:38:35 +0000

    retirando sidebar de calendarios de todas as telas. colocando cadastro de salas em ajax

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,2 +1,9 @@
 class ClassroomsController < ResourceController::Base
+  new_action.wants.js { render :layout => false }
+
+  create do
+    flash "Classroom successfully created!"
+    wants.js
+    failure.wants.js { render :action => 'new', :layout => false }
+  end
 end
--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,4 +1,7 @@
 class Classroom < ActiveRecord::Base
+  validates_presence_of :name
+  validates_numericality_of :capacity, :allow_nil => false, :greater_than => 0
+  
   def to_s
     name
   end
