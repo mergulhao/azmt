de5530ed6ba6e84c23b6255f5578073e73c380a0
mergulhao <mergulhao>
2008-07-15 04:41:44 +0000

    cadastro de salas agora 100% ajax e testado no rspec

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,9 +1,19 @@
 class ClassroomsController < ResourceController::Base
   new_action.wants.js { render :layout => false }
 
+  edit do
+    wants.js { render :layout => false }
+  end
+
+  update do
+    flash "Classroom successfully updated!"
+    wants.js { render :template => 'common/redirect_to_collection' }
+    failure.wants.js { render :action => 'edit', :layout => false }
+  end
+  
   create do
     flash "Classroom successfully created!"
-    wants.js
+    wants.js { render :template => 'common/redirect_to_collection' }
     failure.wants.js { render :action => 'new', :layout => false }
   end
 end
