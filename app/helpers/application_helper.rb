b3e07867258dd1f21952ff92aa47279cd422d73e
mergulhao <mergulhao>
2008-07-24 04:19:16 +0000

    mais ajustes em layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,32 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def title
     'Default title! Override it!'
+  end
+  
+  def create_button_and_cancel_link_to_clear_box_form
+    submit_button(_('Create')) +
+      link_to_clear_box_form
+  end
+  
+  def update_button_and_cancel_link_to_clear_box_form
+    submit_button(_('Update')) +
+      link_to_clear_box_form
+  end
+  
+  def submit_button(label)
+    result = []
+    result << '<label class="x-small lblbutton">'
+    result << submit_tag(label, :class => "button")
+    result << '</label>'
+    result.join
+  end
+  
+  def link_to_clear_box_form
+    result = []
+    result << '<label class="x-small" style="text-align: right;">'
+    result << link_to_function('Cancelar', 'clearBoxForm()')
+    result << '</label>'
+    result.join
   end
   
   def flash_notice
