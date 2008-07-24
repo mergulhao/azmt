ba9d3d1b4844a9f653494048abc02daaeaccc333
mergulhao <mergulhao>
2008-07-24 04:19:14 +0000

    mais ajustes em layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def title
+    'Default title! Override it!'
+  end
+  
+  def flash_notice
+    if flash[:notice]
+      content_tag :p, flash[:notice]
+    end
+  end
 end
