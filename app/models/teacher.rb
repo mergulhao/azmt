4275d5508675beb5bab5a90f169b6bc8d4e1d84c
mergulhao <mergulhao@76623a5a-d52f-0410-8f39-fa83289919e8>
2008-08-20 20:45:12 +0000

    Incluindo disciplinas no cadastro de formacao
    git-svn-id: svn+vpsssh://en21.org/svn/azmt/trunk@335 76623a5a-d52f-0410-8f39-fa83289919e8

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,5 +1,5 @@
 class Teacher < ActiveRecord::Base
-  has_and_belongs_to_many :can_teach, :class_name => 'Discipline'
+  has_and_belongs_to_many :can_teach, :class_name => 'Discipline', :order => 'name'
   
   def to_s
     name
