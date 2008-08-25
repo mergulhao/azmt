b3bf3b295ef997d3dd35444120bff34c370a96b4
mergulhao <mergulhao>
2008-08-25 04:03:20 +0000

    Fazendo salvar aulas criadas na nova tela

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -2,6 +2,10 @@ class ClassesController < ResourceController::Base
   create do
     wants.html { redirect_to edit_object_url(object) }
   end
+  
+  update do
+    wants.html { redirect_to edit_object_url(object) }
+  end
 
   update.before do
     (params[:classe][:existing_lesson_attributes] ||= {}) if params[:classe]
