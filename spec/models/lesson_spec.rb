e592b1268e0bbc2353cb7551c1583d792e0c26cc
mergulhao <mergulhao>
2008-08-25 04:03:24 +0000

    cor das aulas no calendario de aulas

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -31,15 +31,21 @@ describe Lesson do
   end
   
   describe ".to_s" do
-    it "should return the lesson name" do 
-      lessons(:trigonometry).to_s.should eql('Math - Trigonometry')
-      lessons(:trigonometry).name.should eql('Trigonometry')
+    it "should return the discipline name" do 
+      lessons(:trigonometry).to_s.should eql('Math')
     end
   end
   
   describe ".to_calendar" do 
     it "should return calendar_entry" do
-      lessons(:trigonometry).to_calendar.should eql('18:00 - Math - Trigonometry')
+      lessons(:trigonometry).to_calendar.should eql('18:00 - Math')
     end
   end
+  
+  describe ".color" do
+    it "should return course color" do
+      lessons(:trigonometry).color.should eql('12f5b9')
+    end
+  end
+ 
 end
