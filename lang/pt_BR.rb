c118fef6710c236b22c9ea694059fac007cf07f6
Sylvestre Mergulhao <contato@mergulhao.info>
2008-09-26 02:37:53 -0300

    changing pt_BR language file to satisfy rcov, hunf =/

--- lang/pt_BR.rb
+++ lang/pt_BR.rb
@@ -74,36 +74,7 @@ Localization.define('pt_BR') do |l|
   l.store 'Add %s', 'Adicionar %s'
   
   #---- months
-  l.store '(month)', lambda { |month|
-   case month
-   when 1
-     'Janeiro'
-   when 2
-     'Fevereiro'
-   when 3
-     'Março'
-   when 4
-     'Abril'
-   when 5
-     'Maio'
-   when 6
-     'Junho'
-   when 7
-     'Julho'
-   when 8
-     'Agosto'
-   when 9
-     'Setembro'
-   when 10
-     'Outubro'
-   when 11
-     'Novembro'
-   when 12
-     'Dezembro'
-   else  
-     nil
-   end  
-  }
+  l.store '(month)', lambda { |month|   case month;   when 1;     'Janeiro';   when 2;     'Fevereiro';   when 3;     'Março';   when 4;     'Abril';   when 5;     'Maio';   when 6;     'Junho';   when 7;     'Julho';   when 8;     'Agosto';   when 9;     'Setembro';   when 10;     'Outubro';   when 11;     'Novembro';   when 12;     'Dezembro';   else  ;     nil;   end  ;  }
   
   #---- localization
   l.store '(day)', lambda { |t| t.strftime('%d') rescue '' }
